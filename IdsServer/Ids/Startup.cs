514247ecd9a273cb5ffbafb3e33814ab9670e0d0
Enrique Souza <enriquepdsz@gmail.com>
2021-04-30 20:18:43 -0300

    Updated the IdS with the entity framework.

--- IdsServer/Ids/Startup.cs
+++ IdsServer/Ids/Startup.cs
@@ -1,6 +1,8 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,16 +10,36 @@ namespace Ids
 {
     public class Startup
     {
+        private readonly IWebHostEnvironment _environment;
+        private readonly IConfiguration _configuration;
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _environment = environment;
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddIdentityServer()
-                .AddInMemoryClients(Config.Clients)
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiResources(Config.ApiResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddTestUsers(Config.Users)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder => builder.UseSqlite(connectionString,
+                        opt => opt.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder => builder.UseSqlite(connectionString,
+                        opt => opt.MigrationsAssembly(migrationsAssembly));
+
+                    options.EnableTokenCleanup = true;
+                })
                 .AddDeveloperSigningCredential();
 
             services.AddControllersWithViews();
