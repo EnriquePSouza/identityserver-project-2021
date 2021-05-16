8fc3a645a9c4db6a5342b46f45a127adbf961a95
Enrique Souza <enriquepdsz@gmail.com>
2021-05-15 23:39:23 -0300

    Updated the oauth with client authentication

---
+++ opt2/oauth/Server/Server/Controllers/OAuthController.cs
@@ -0,0 +1,91 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace Server.Controllers
+{
+    public class OAuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Authorize(
+            string response_type, // O tipo de fluxo de autorização que será usado
+            string client_id,
+            string redirect_uri,
+            string scope, // As informações que eu desejo checar
+            string state) // valor randomico que indica para qual client voltaremos
+        {
+            // ?a=foo&b=bar
+            var query = new QueryBuilder();
+            query.Add("redirectUri", redirect_uri);
+            query.Add("state", state);
+
+            return View(model: query.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Authorize(
+            string username,
+            string redirectUri,
+            string state)
+        {
+            const string code = "JKEISHEHDF";
+
+            var query = new QueryBuilder();
+            query.Add("code", code);
+            query.Add("state", state); 
+
+            return Redirect($"{redirectUri}{query.ToString()}");
+        }
+
+        public IActionResult Token(
+            string grant_type, // fluxo para pedir o token
+            string code, // confirmação da autenticação
+            string redirect_uri,
+            string client_id)
+        {
+            // some mechanism for validating the code
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "some_id"),
+                new Claim("granny", "cookie")
+            };
+
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var signingCredentials = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audiance,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials);
+
+            var access_token = new JwtSecurityTokenHandler().WriteToken(token);
+
+            var responseObject = new
+            {
+                access_token,
+                token_type = "Bearer",
+                raw_claim = "oauthTutorial"
+            };
+
+            // var responseJson = JsonConvert.SerializeObject(responseObject);
+            // var responseBytes = Encoding.UTF8.GetBytes(responseJson);
+
+            // await Response.Body.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+            return Ok(responseObject);
+        }
+    }
+}
