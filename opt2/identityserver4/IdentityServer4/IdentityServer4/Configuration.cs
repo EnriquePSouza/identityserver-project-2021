460bc8d0f93c82f44bcac005271d451f0a941771
EnriquePSouza <enriquepdsz@gmail.com>
2021-05-31 16:09:25 -0300

    Added the basics for IdentityServer4

---
+++ opt2/identityserver4/IdentityServer4/IdentityServer4/Configuration.cs
@@ -0,0 +1,31 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public static class Configuration
+    {
+        public static IEnumerable<ApiResource> GetApis() =>
+            new List<ApiResource> { 
+                new ApiResource("ApiOne") //declaração da api
+            };
+
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client_id",
+                    ClientSecrets = { new Secret("client_secret".ToSha256()) }, // Esse é apenas um exemplo, o segredo precisa ser complexo, vc pode usar um guid como segredo.
+                    
+                    AllowedGrantTypes = GrantTypes.ClientCredentials, // Como pegará o access token
+
+                    AllowedScopes = { "ApiOne" } // Diz qual api esse client pode acessar
+                }
+            };
+    }
+}
---
+++ opt2/identityserver4/IdentityServer4/IdentityServer4/Startup.cs
@@ -0,0 +1,44 @@
+using IdentityServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // O identityServer4 é uma toolbox que fornece a infraestrutura para criação de um servidor de authenticação OpenId.
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(Configuration.GetApis())
+                .AddInMemoryClients(Configuration.GetClients())
+                .AddDeveloperSigningCredential();
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
